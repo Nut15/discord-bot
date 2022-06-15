4438417b97bb1abe1f7b4e551df8814145e84f38
Nora D <am_qi7777@outlook.com>
2022-06-15 19:17:21 +1000

    ok

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ async def on_command_error(ctx,error):
         await ctx.message.delete()
 
 #command 'kill'
-@client.command(name='kill',brief='kill someone.',description='kill anyone you want. However the chance for them to die is 3%. Has cooldown for half a day (12 hrs). Kills are anonymous')
+@client.command(name='kill',brief='kill someone.',description='kill anyone you want. However the chance for them to die is 5%. Has cooldown for half a day (12 hrs). Kills are anonymous')
 @commands.cooldown(1, 43200, commands.BucketType.user)
 async def kill(ctx, member:discord.Member):
     chance = random.randint(0,20)
